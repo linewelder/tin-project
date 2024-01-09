5d6fcb1c9f5706eb980d223f4fdf0a0955952584
linewelder <redalerm@gmail.com>
2024-01-09 16:12:19 +0100

    Add layout and CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
 function App() {
-  return <div>Hello World!</div>;
+    return (
+        <>
+            <header>
+                <div class="header-wrapper">
+                    <div>
+                        <a class="navlink-selected" href="tournaments/index.html">Tournaments</a>
+                        <a href="participants/index.html">Participants</a>
+                        <a href="categories/index.html">Categories</a>
+                    </div>
+                    <div>
+                        <span id="user-name">Jan Nowak</span>
+                        <button id="logout-btn">Logout</button>
+                    </div>
+                </div>
+            </header>
+
+            <div class="wrapper">
+                <h2>Welcome</h2>
+                <p>Hello, world!</p>
+            </div>
+        </>
+    );
 }
 
 export default App;
