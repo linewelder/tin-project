dcc101705444ec84bdbf2f8e401ae83cf0669083
linewelder <redalerm@gmail.com>
2024-01-10 05:57:48 +0100

    Move Home.jsx and About.jsx to pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Route, NavLink, Routes, BrowserRouter } from "react-router-dom";
 import { FormattedMessage } from "react-intl";
-import Home from "./Home";
-import About from "./About";
+import Home from "./pages/Home";
+import About from "./pages/About";
 
 function App() {
     return (
