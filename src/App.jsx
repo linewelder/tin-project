94ddc1c3e28ff0b0a99dca52f000aeccb172d25a
linewelder <redalerm@gmail.com>
2024-01-10 06:11:15 +0100

    Use createBrowserRouter instead of BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,57 @@
-import { Route, NavLink, Routes, BrowserRouter } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Outlet, NavLink } from "react-router-dom";
 import { FormattedMessage } from "react-intl";
+
 import Home from "./pages/Home";
 import About from "./pages/About";
 
+const Layout = () => (
+    <>
+        <header>
+            <div className="header-wrapper">
+                <div>
+                    <NavLink to="/">
+                        <FormattedMessage id="nav.home" />
+                    </NavLink>
+                    <NavLink to="/about">
+                        <FormattedMessage id="nav.about" />
+                    </NavLink>
+                </div>
+                <div>
+                    <span id="user-name">
+                        Jan Nowak
+                    </span>
+                    <button id="logout-btn">
+                        <FormattedMessage id="button.logout" />
+                    </button>
+                </div>
+            </div>
+        </header>
+        <div className="wrapper">
+            <Outlet />
+        </div>
+    </>
+);
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Layout />,
+        children: [
+            {
+                path: "/",
+                element: <Home />,
+            },
+            {
+                path: "/about",
+                element: <About />,
+            },
+        ],
+    },
+]);
+
 function App() {
     return (
-        <BrowserRouter>
-            <header>
-                <div className="header-wrapper">
-                    <div>
-                        <NavLink to="/">
-                            <FormattedMessage id="nav.home" />
-                        </NavLink>
-                        <NavLink to="/about">
-                            <FormattedMessage id="nav.about" />
-                        </NavLink>
-                    </div>
-                    <div>
-                        <span id="user-name">
-                            Jan Nowak
-                        </span>
-                        <button id="logout-btn">
-                            <FormattedMessage id="button.logout" />
-                        </button>
-                    </div>
-                </div>
-            </header>
-
-            <div className="wrapper">
-                <Routes>
-                    <Route path="/" element={<Home/>}/>
-                    <Route path="/about" element={<About/>}/>
-                </Routes>
-            </div>
-        </BrowserRouter>
+        <RouterProvider router={router} />
     );
 }
 
