82c7f2e61d839eebe2d69635efe2b1e458f54456
linewelder <redalerm@gmail.com>
2024-01-18 22:07:52 +0100

    client: Implement /signup page

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,7 +1,39 @@
+import { useState } from "react";
 import { FormattedMessage } from "react-intl";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 function Signup() {
+    const [inputs, setInputs] = useState({
+        email: "",
+        firstName: "",
+        lastName: "",
+        password: "",
+        passwordAgain: ""
+    });
+
+    const [error, setError] = useState(null);
+
+    const navigate = useNavigate();
+
+    const onInputsChanged = (e) => {
+        setInputs({
+            ...inputs,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            await axios.post("http://localhost:8800/api/auth/register", inputs);
+            navigate("/");
+        } catch (error) {
+            setError(error);
+        }
+    }
+
     return (
         <>
             <h2><FormattedMessage id="page.signup.title" /></h2>
@@ -10,30 +42,37 @@ function Signup() {
                 <label for="email">
                     <FormattedMessage id="label.email" />
                 </label>
-                <input id="email" type="text" />
+                <input id="email" type="email" name="email"
+                    value={inputs.email} onChange={onInputsChanged} />
 
-                <label for="first-name">
+                <label for="firstName">
                     <FormattedMessage id="label.first-name" />
                 </label>
-                <input id="first-name" type="text" />
+                <input id="firstName" type="text" name="firstName"
+                    value={inputs.firstName} onChange={onInputsChanged} />
 
-                <label for="last-name">
+                <label for="lastName">
                     <FormattedMessage id="label.last-name" />
                 </label>
-                <input id="last-name" type="text" />
+                <input id="lastName" type="text" name="lastName"
+                    value={inputs.lastName} onChange={onInputsChanged} />
 
                 <label for="password">
                     <FormattedMessage id="label.password" />
                 </label>
-                <input id="password" type="password" />
+                <input id="password" type="password" name="password"
+                    value={inputs.password} onChange={onInputsChanged} />
 
-                <label for="password-again">
+                <label for="passwordAgain">
                     <FormattedMessage id="label.password-again" />
                 </label>
-                <input id="password-again" type="password" />
+                <input id="passwordAgain" type="password" name="passwordAgain"
+                    value={inputs.passwordAgain} onChange={onInputsChanged} />
 
-                <button><FormattedMessage id="button.signup" /></button>
+                <button onClick={onSubmit}><FormattedMessage id="button.signup" /></button>
             </form>
+
+            {error && <p className="error">{error + ""}</p>}
 
             <p className="right-aligned">
                 <Link to="/login"><FormattedMessage id="page.login.title" /></Link>
