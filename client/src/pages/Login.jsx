cc55d37e0a1ca42ce5b1db3292b465e47025fa3f
linewelder <redalerm@gmail.com>
2024-01-19 13:54:22 +0100

    client: Implement login page logic

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,59 @@
-import { FormattedMessage } from "react-intl";
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { FormattedMessage, useIntl } from "react-intl";
+import { Link, useNavigate } from "react-router-dom";
+import api from "../api.js";
 
 function Login() {
+    const [inputs, setInputs] = useState({
+        email: "",
+        password: ""
+    });
+    const [errors, setErrors] = useState([]);
+
+    const navigate = useNavigate();
+    const intl = useIntl();
+
+    const onInputsChanged = (e) => {
+        setInputs({
+            ...inputs,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    // Zwraca listę błędów walidacji.
+    const validate = () => {
+        const errors = [];
+
+        const validateNotEmpty = (name) => {
+            if (inputs[name].length === 0) {
+                errors.push(intl.formatMessage({ id: `error.${name}.empty` }));
+            }
+        };
+
+        validateNotEmpty("email");
+        validateNotEmpty("password");
+
+        return errors;
+    };
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+
+        const errors = validate();
+        if (errors.length > 0) {
+            setErrors(errors);
+            return;
+        }
+
+        const [result, error] = await api.post("/auth/login", inputs);
+        if (result) {
+            navigate("/");
+        } else {
+            errors.push(error.format(intl));
+            setErrors(errors);
+        }
+    };
+
     return (
         <>
             <h2><FormattedMessage id="page.login.title" /></h2>
@@ -10,15 +62,23 @@ function Login() {
                 <label htmlFor="email">
                     <FormattedMessage id="label.email" />
                 </label>
-                <input id="email" type="text" />
+                <input id="email" type="text" name="email"
+                    value={inputs.email} onChange={onInputsChanged} />
 
                 <label htmlFor="password">
                     <FormattedMessage id="label.password" />
                 </label>
-                <input id="password" type="password" />
+                <input id="password" type="password" name="password"
+                    value={inputs.password} onChange={onInputsChanged} />
 
-                <button><FormattedMessage id="button.login" /></button>
+                <button onClick={onSubmit}><FormattedMessage id="button.login" /></button>
             </form>
+
+            <ul className="error">
+                {errors.map((error, index) => (
+                    <li key={index}>{error}</li>
+                ))}
+            </ul>
 
             <p className="right-aligned">
                 <Link to="/signup"><FormattedMessage id="page.signup.title" /></Link>
