366a34bce39a8c5b02dba0759278dfc0b258e742
linewelder <redalerm@gmail.com>
2024-01-20 14:37:34 +0100

    api: Create authorization middleware

---
+++ api/middleware.js
@@ -0,0 +1,33 @@
+import jwt from "jsonwebtoken";
+
+/**
+ * Sprawdza, czy klient jest zalogowany (nagłówek Authorization).
+ * Dodaje do obiektu requesta informacje zawarte w tokenie (req.claims).
+ *
+ * @param {*} admin Czy klient uprawnienia administratorana są wymagane.
+ */
+export function authorize({ admin } = { admin: false }) {
+    return (req, res, next) => {
+        const header = req.headers.authorization || "";
+        const [scheme, token] = header.split(/\s+/);
+        if (!scheme || scheme.toLowerCase() !== "bearer" || !token) {
+            res.status(401).json({ error: "not-authenticated"});
+            return;
+        }
+
+        jwt.verify(token, "mysecret", (err, claims) => {
+            if (err) {
+                res.status(401).json({ error: "invalid-token" });
+                return;
+            }
+
+            if (admin && !claims.admin) {
+                res.status(403).json({ error: "access-denied" });
+                return;
+            }
+
+            req.claims = claims;
+            next();
+        });
+    }
+}
