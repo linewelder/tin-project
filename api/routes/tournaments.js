d60d7e54be6354dee873ee012ba406dcde01c450
linewelder <redalerm@gmail.com>
2024-01-22 20:10:09 +0100

    api: Create separate controller for tournament participants

--- api/routes/tournaments.js
+++ api/routes/tournaments.js
@@ -1,14 +1,16 @@
 import express from "express";
 import asyncHandler from "express-async-handler";
-import { addNew, getAll, getOne, getParticipants, update } from "../controllers/tournament.js";
+import { addNew, getAll, getOne, update } from "../controllers/tournament.js";
+import { getParticipants } from "../controllers/tournamentParticipant.js";
 import { authorize } from "../middleware.js";
 
 const router = express.Router();
 
 router.get("/", asyncHandler(getAll));
 router.get("/:id", asyncHandler(getOne));
-router.get("/:id/participants", asyncHandler(getParticipants));
 router.post("/", authorize(), asyncHandler(addNew));
 router.put("/:id", authorize(), asyncHandler(update));
+
+router.get("/:id/participants", asyncHandler(getParticipants));
 
 export default router;
