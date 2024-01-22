e426ccd39e6993d7aa1b14ea8ef35a92af7e55a1
linewelder <redalerm@gmail.com>
2024-01-22 21:35:10 +0100

    api: Add participant endpoints

--- api/controllers/participant.js
+++ api/controllers/participant.js
@@ -1,5 +1,6 @@
+import joi from "joi";
 import db from "../db.js";
-import { getPaginationParams } from "../validation.js";
+import { getPaginationParams, tryValidate } from "../validation.js";
 
 export async function getAll(req, res) {
     const [first, count] = getPaginationParams(req);
@@ -55,4 +56,76 @@ export async function getOne(req, res) {
     }));
 
     res.json(participant);
+}
+
+const newParticipantSchema = joi.object({
+    firstName: joi.string()
+        .max(30)
+        .required(),
+    lastName: joi.string()
+        .max(30)
+        .required(),
+});
+
+export async function addNew(req, res) {
+    const data = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+    };
+    if (!tryValidate(res, data, newParticipantSchema)) {
+        return;
+    }
+
+    const result = await db.query(
+        "INSERT INTO Participant(FirstName, LastName) VALUES (?)",
+        [[data.firstName, data.lastName]]);
+    data.id = result.insertId;
+
+    res.json(data);
+}
+
+export async function deleteOne(req, res) {
+    const id = req.params.id;
+    if (id < 1) return res.status(404).json({ "error": "not-found" });
+
+    const rows = await db.query("DELETE FROM Participant WHERE IdParticipant = ?", [id]);
+    if (rows.rowCount === 0) {
+        res.status(404).json({ "error": "not-found" });
+        return;
+    }
+
+    res.status(204).end();
+};
+
+const updateParticipantSchema = joi.object({
+    firstName: joi.string()
+        .max(30)
+        .required(),
+    lastName: joi.string()
+        .max(30)
+        .required(),
+});
+
+export async function update(req, res) {
+    const id = req.params.id;
+    if (id < 1) return res.status(404).json({ error: "not-found" });
+
+    const data = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+    };
+    if (!tryValidate(res, data, updateParticipantSchema)) {
+        return;
+    }
+
+    const rows = await db.query(
+        "UPDATE Participant " +
+        "SET FirstName=?, LastName=? " +
+        "WHERE IdParticipant = ?",
+        [data.firstName, data.lastName, id]);
+    if (rows.rowCount === 0) {
+        res.status(404).json({ "error": "not-found" });
+        return;
+    }
+    res.json({});
 }
--- api/routes/participants.js
+++ api/routes/participants.js
@@ -1,10 +1,14 @@
 import express from "express";
 import asyncHandler from "express-async-handler";
-import { getAll, getOne } from "../controllers/participant.js";
+import { getAll, getOne, addNew, deleteOne, update } from "../controllers/participant.js";
+import { authorize } from "../middleware.js";
 
 const router = express.Router();
 
 router.get("/", asyncHandler(getAll));
 router.get("/:id", asyncHandler(getOne));
+router.post("/", authorize({ admin: true }), asyncHandler(addNew));
+router.delete("/:id", authorize({ admin: true }), asyncHandler(deleteOne));
+router.put("/:id", authorize({ admin: true }), asyncHandler(update));
 
 export default router;
