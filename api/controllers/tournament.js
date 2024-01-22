fcc7e1939bdfa5cca71cd9ba38a9eddfed54ba75
linewelder <redalerm@gmail.com>
2024-01-22 20:33:57 +0100

    api: Move getParticipants back to tournament controller

--- api/controllers/tournament.js
+++ api/controllers/tournament.js
@@ -62,6 +62,26 @@ export async function getOne(req, res) {
     res.json(tournament);
 }
 
+export async function getParticipants(req, res) {
+    const id = req.params.id;
+    if (id < 1) return res.status(404).json({ "error": "not-found" });
+
+    const rows = await db.query(
+        "SELECT Participant.IdParticipant, FirstName, LastName, Time " +
+        "FROM TournamentParticipant " +
+        "JOIN Participant ON Participant.IdParticipant = TournamentParticipant.IdParticipant " +
+        "WHERE IdTournament = ? ",
+        [id]);
+
+    const participants = rows.map(row => ({
+        id: row.IdParticipant,
+        firstName: row.FirstName,
+        lastName: row.LastName,
+        result: row.Time,
+    }));
+    res.json(participants);
+}
+
 const newTournamentSchema = joi.object({
     name: joi.string()
         .max(50)
--- api/routes/tournaments.js
+++ api/routes/tournaments.js
@@ -1,16 +1,14 @@
 import express from "express";
 import asyncHandler from "express-async-handler";
-import { addNew, getAll, getOne, update } from "../controllers/tournament.js";
-import { getParticipants } from "../controllers/tournamentParticipant.js";
+import { addNew, getAll, getOne, getParticipants, update } from "../controllers/tournament.js";
 import { authorize } from "../middleware.js";
 
 const router = express.Router();
 
 router.get("/", asyncHandler(getAll));
 router.get("/:id", asyncHandler(getOne));
+router.get("/:id/participants", asyncHandler(getParticipants));
 router.post("/", authorize(), asyncHandler(addNew));
 router.put("/:id", authorize(), asyncHandler(update));
-
-router.get("/:id/participants", asyncHandler(getParticipants));
 
 export default router;
