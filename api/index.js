7fede11246aaf4d365e0eb5a71c6c91535026167
linewelder <redalerm@gmail.com>
2024-01-22 17:20:25 +0100

    api: Add custom error handler

--- api/index.js
+++ api/index.js
@@ -10,12 +10,18 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
 app.use("/api/auth", authRouter);
 app.use("/api/categories", categoriesRouter);
 app.use("/api/tournaments", tournamentsRouter);
 app.use("/api/participants", participantsRouter);
 app.use("/api/users", usersRouter);
 app.use("*", (_, res) => { res.status(404).json({ "error": "path-not-found" }); });
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ error: err.message, formattable: false });
+});
 
 const port = 8800;
 app.listen(port, () => {
