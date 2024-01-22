bf585c2cf640d7e4fda86a45393e62f2eae8eb9c
linewelder <redalerm@gmail.com>
2024-01-22 05:26:10 +0100

    client: Display user's tournaments

--- client/src/pages/Tournaments.jsx
+++ client/src/pages/Tournaments.jsx
@@ -1,25 +1,68 @@
 import { FormattedDate, FormattedMessage } from "react-intl";
 import { Link } from "react-router-dom";
-import { useEffect, useState } from "react";
-import { usePagination } from "../apiContext.jsx";
+import { useContext, useEffect, useState } from "react";
+import { ApiContext, usePagination } from "../apiContext.jsx";
 import Pagination from "../components/Pagination.jsx";
 
 export default function Categories() {
     const [ error, setError ] = useState(null);
     useEffect(() => { if (error) throw error; }, [error]);
 
+    const api = useContext(ApiContext);
+    const userId = api.currentUser?.id;
+
+    const usersTournaments = userId
+        ? usePagination(`/users/${userId}/tournaments`, 3, setError)
+        : null;
     const tournaments = usePagination("/tournaments", 3, setError);
 
     return (
         <>
+            {usersTournaments && (
+                <>
+                    <h2><FormattedMessage id="page.tournaments.users" /></h2>
+                    <Link to="create" className="button-link">
+                        <FormattedMessage id="button.add-new" />
+                    </Link>
+
+                    <Pagination pagination={usersTournaments} />
+                    <table>
+                        <thead>
+                            <tr>
+                                <th><FormattedMessage id="table.tournament.id" /></th>
+                                <th><FormattedMessage id="table.tournament.name" /></th>
+                                <th><FormattedMessage id="table.tournament.date" /></th>
+                                <th><FormattedMessage id="table.tournament.category" /></th>
+                                <th><FormattedMessage id="table.tournament.status" /></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {usersTournaments.elements.map(tournament =>
+                                <tr key={tournament.id}>
+                                    <td>{tournament.id}</td>
+                                    <td>
+                                        <Link to={`${tournament.id}`}>
+                                            {tournament.name}
+                                        </Link>
+                                    </td>
+                                    <td>
+                                        <FormattedDate value={tournament.date} />
+                                    </td>
+                                    <td>{tournament.category}</td>
+                                    <td>
+                                        <FormattedMessage id={
+                                            `table.tournament.status.${tournament.isClosed ? "closed" : "open" }`} />
+                                    </td>
+                                </tr>
+                            )}
+                        </tbody>
+                    </table>
+                </>
+            )}
+
             <h2><FormattedMessage id="page.tournaments.all" /></h2>
 
-            <Link to="create" className="button-link">
-                <FormattedMessage id="button.add-new" />
-            </Link>
-
             <Pagination pagination={tournaments} />
-
             <table>
                 <thead>
                     <tr>
